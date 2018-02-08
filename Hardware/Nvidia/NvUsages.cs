94d08ded9c6ca9900dcf8276a80cdbc246d4458b
Matteo Fabbri <matteo@phascode.org>
2018-02-08 10:15:26 +0100

    splitted Nvidia files

---
+++ Hardware/Nvidia/NvUsages.cs
@@ -0,0 +1,22 @@
+﻿/*
+ 
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ 
+  Copyright (C) 2009-2012 Michael Möller <mmoeller@openhardwaremonitor.org>
+	Copyright (C) 2011 Christian Vallières
+ 
+*/
+
+using System.Runtime.InteropServices;
+
+namespace OpenHardwareMonitor.Hardware.Nvidia
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 8)]
+    public struct NvUsages
+    {
+        public uint Version;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = NVAPI.MAX_USAGES_PER_GPU)] public uint[] Usage;
+    }
+}
