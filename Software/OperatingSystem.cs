openhardwaremonitor/openhardwaremonitor

commit 66e69c4c25ef2554b3ed44378dc5fc465ab47781

Pr/30 (#32)
* Unified operating system type detection

* Revert "Unified operating system type detection"

This reverts commit 3edf0edfceaca259e041e23b9930d78531366fd5.

* Introduced OperatingSystem.IsLinux instead of constant check on Os version.

* Misc

Software/OperatingSystem.cs
@@ -1,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace OpenHardwareMonitor.Software
+{
+    public static class OperatingSystem
+    {
+        static OperatingSystem()
+        {
+            // The operating system doesn't change during execution so let's query it just one time.
+            var platform = Environment.OSVersion.Platform;
+            IsLinux = platform == PlatformID.Unix || platform == PlatformID.MacOSX;
+
+
+            if (IntPtr.Size == 8)
+                Is64Bit = true;
+
+            try
+            {
+                var result = IsWow64Process(Process.GetCurrentProcess().Handle, out bool wow64Process);
+
+                Is64Bit = result && wow64Process;
+            }
+            catch (EntryPointNotFoundException)
+            {
+                Is64Bit = false;
+            }
+        }
+
+        public static bool Is64Bit { get; }
+
+        public static bool IsLinux { get; }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool IsWow64Process(IntPtr hProcess, out bool wow64Process);
+    }
+}

Hardware/OperatingSystem.cs
@@ -1,38 +1,0 @@
-﻿/*
- 
-  This Source Code Form is subject to the terms of the Mozilla Public
-  License, v. 2.0. If a copy of the MPL was not distributed with this
-  file, You can obtain one at http://mozilla.org/MPL/2.0/.
- 
-  Copyright (C) 2012 Michael Möller <mmoeller@openhardwaremonitor.org>
-	
-*/
-
-using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-
-namespace OpenHardwareMonitor.Hardware {
-  internal static class OperatingSystem {
-
-    public static bool Is64BitOperatingSystem() {
-      if (IntPtr.Size == 8)
-        return true;
-
-      try {
-        bool wow64Process;
-        bool result = IsWow64Process(
-          Process.GetCurrentProcess().Handle, out wow64Process);
-
-        return result && wow64Process;
-      } catch (EntryPointNotFoundException) {
-        return false;
-      }
-    }
-
-    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    private static extern bool IsWow64Process(IntPtr hProcess,
-      out bool wow64Process);
-  }
-}

Hardware/CPU/AMD10CPU.cs
@@ -144,8 +144,8 @@ namespace OpenHardwareMonitor.Hardware.CPU {
 
       // the file reader for lm-sensors support on Linux
       temperatureStream = null;
-      int p = (int)Environment.OSVersion.Platform;
-      if ((p == 4) || (p == 128)) {
+      
+      if (Software.OperatingSystem.IsLinux) {
         string[] devicePaths = Directory.GetDirectories("/sys/class/hwmon/");
         foreach (string path in devicePaths) {
           string name = null;

Hardware/HDD/HarddriveGroup.cs
@@ -24,8 +24,8 @@ namespace OpenHardwareMonitor.Hardware.HDD {
       new List<AbstractHarddrive>();
 
     public HarddriveGroup(ISettings settings) {
-      int p = (int)Environment.OSVersion.Platform;
-      if (p == 4 || p == 128) return;
+
+      if (Software.OperatingSystem.IsLinux) return;
 
       ISmart smart = new WindowsSmart();
 

Hardware/Heatmaster/HeatmasterGroup.cs
@@ -67,9 +67,7 @@ namespace OpenHardwareMonitor.Hardware.Heatmaster {
     public HeatmasterGroup(ISettings settings) {
       
       // No implementation for Heatmaster on Unix systems
-      int p = (int)Environment.OSVersion.Platform;
-      if ((p == 4) || (p == 128))
-        return;
+      if (Software.OperatingSystem.IsLinux) return;
 
       string[] portNames = GetRegistryPortNames();      
       for (int i = 0; i < portNames.Length; i++) {

Hardware/Mainboard/Mainboard.cs
@@ -50,8 +50,8 @@ namespace OpenHardwareMonitor.Hardware.Mainboard {
         new Identifier(Identifier, "name").ToString(), name);
 
       ISuperIO[] superIO;
-      int p = (int)Environment.OSVersion.Platform;
-      if ((p == 4) || (p == 128)) {
+
+      if (Software.OperatingSystem.IsLinux) {
         this.lmSensors = new LMSensors();
         superIO = lmSensors.SuperIO;
       } else {

Hardware/Opcode.cs
@@ -19,8 +19,7 @@ namespace OpenHardwareMonitor.Hardware {
     private static ulong size;
 
     public static void Open() {  
-      int p = (int)Environment.OSVersion.Platform;
-            
+
       byte[] rdtscCode;
       byte[] cpuidCode;
       if (IntPtr.Size == 4) {
@@ -29,7 +28,7 @@ namespace OpenHardwareMonitor.Hardware {
       } else {
         rdtscCode = RDTSC_64;
         
-        if ((p == 4) || (p == 128)) { // Unix
+        if (Software.OperatingSystem.IsLinux) { // Unix
           cpuidCode = CPUID_64_LINUX;
         } else { // Windows
           cpuidCode = CPUID_64_WINDOWS;
@@ -38,7 +37,7 @@ namespace OpenHardwareMonitor.Hardware {
       
       size = (ulong)(rdtscCode.Length + cpuidCode.Length);
 
-      if ((p == 4) || (p == 128)) { // Unix   
+      if (Software.OperatingSystem.IsLinux) { // Unix   
         Assembly assembly = 
           Assembly.Load("Mono.Posix, Version=2.0.0.0, Culture=neutral, " +
           "PublicKeyToken=0738eb9f132ed756");
@@ -81,8 +80,8 @@ namespace OpenHardwareMonitor.Hardware {
       Rdtsc = null;
       Cpuid = null;
       
-      int p = (int)Environment.OSVersion.Platform;
-      if ((p == 4) || (p == 128)) { // Unix
+
+      if (Software.OperatingSystem.IsLinux) { // Unix
         Assembly assembly =
           Assembly.Load("Mono.Posix, Version=2.0.0.0, Culture=neutral, " +
           "PublicKeyToken=0738eb9f132ed756");

Hardware/RAM/RAMGroup.cs
@@ -18,8 +18,8 @@ namespace OpenHardwareMonitor.Hardware.RAM {
     public RAMGroup(SMBIOS smbios, ISettings settings) {
 
       // No implementation for RAM on Unix systems
-      int p = (int)Environment.OSVersion.Platform;
-      if ((p == 4) || (p == 128)) {
+
+      if (Software.OperatingSystem.IsLinux) {
         hardware = new Hardware[0];
         return;
       }

LibreHardwareMonitor/LibreHardwareMonitor

commit 4dfee18289c009ab7a46d7c49eb0b3b3f00885a2

Fix 64bit detection, broken since 66e69c4c25ef2554b3ed44378dc5fc465ab47781.
This commit restores the previous logic of accepting an 8-byte intptr as evidence
enough for 64bit. It also accepts presence of IsWow64Process as evidence of 64bit.
On 64bit, this may return true or false depending on the process. On 32bit this entry
point is not available and an exception will be raised.

Software/OperatingSystem.cs
@@ -2,38 +2,36 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-namespace OpenHardwareMonitor.Software
-{
-    public static class OperatingSystem
-    {
-        static OperatingSystem()
-        {
-            // The operating system doesn't change during execution so let's query it just one time.
-            var platform = Environment.OSVersion.Platform;
-            IsLinux = platform == PlatformID.Unix || platform == PlatformID.MacOSX;
+namespace OpenHardwareMonitor.Software {
+  public static class OperatingSystem {
+    static OperatingSystem() {
+      // The operating system doesn't change during execution so let's query it just one time.
+      var platform = Environment.OSVersion.Platform;
+      IsLinux = platform == PlatformID.Unix || platform == PlatformID.MacOSX;
 
 
-            if (IntPtr.Size == 8)
-                Is64Bit = true;
-
-            try
-            {
-                var result = IsWow64Process(Process.GetCurrentProcess().Handle, out bool wow64Process);
-
-                Is64Bit = result && wow64Process;
-            }
-            catch (EntryPointNotFoundException)
-            {
-                Is64Bit = false;
-            }
+      if (IntPtr.Size == 8) {
+        Is64Bit = true;
+      } else if (!IsLinux) {
+        try {
+          var result = IsWow64Process(Process.GetCurrentProcess().Handle, out bool wow64Process);
+          // If we are still here, this is a 64bit windows; 32bit windows does
+          // not provide IsWow64Process.
+          Is64Bit = true;
         }
-
-        public static bool Is64Bit { get; }
-
-        public static bool IsLinux { get; }
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool IsWow64Process(IntPtr hProcess, out bool wow64Process);
+        catch (EntryPointNotFoundException) {
+          // IsWow64Process is not present on 32 bit:
+          Is64Bit = false;
+        }
+      }
     }
+
+    public static bool Is64Bit { get; }
+
+    public static bool IsLinux { get; }
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool IsWow64Process(IntPtr hProcess, out bool wow64Process);
+  }
 }