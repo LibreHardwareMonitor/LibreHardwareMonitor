38023e7b348a89b82cdc444537efd6434f6622f1
LORDofDOOM <tb@bbz-int.de>
2018-08-25 01:57:12 +0200

    Fix some merge conflicts

--- GUI/HardwareTypeImage.cs
+++ GUI/HardwareTypeImage.cs
@@ -61,6 +61,7 @@ namespace OpenHardwareMonitor.GUI {
             break;
           case HardwareType.Aquacomputer:
             image = Utilities.EmbeddedResources.GetImage("acicon.png");
+            break;
           case HardwareType.NIC:
             image = Utilities.EmbeddedResources.GetImage("nic.png");
             break;
