159886cce101be6ee75d72e4fe34e60d35a25476
Phyxion <marcel@treexy.com>
2018-08-26 10:45:30 +0200

    Line endings

--- GUI/SensorGadget.cs
+++ GUI/SensorGadget.cs
@@ -576,7 +576,8 @@ namespace OpenHardwareMonitor.GUI {
                 case SensorType.Data:
                   format = "{0:F1} GB";
                   break;
-                case SensorType.SmallData:                  format = "{0:F0} MB";
+                case SensorType.SmallData:
+                  format = "{0:F0} MB";
                   break;
                 case SensorType.Factor:
                   format = "{0:F3}";
