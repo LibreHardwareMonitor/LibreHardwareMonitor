f9c3875a9aede436a40af137cf56e107740c5ef3
Nick Babcock <nbabcock19@hotmail.com>
2019-01-01 23:01:19 -0600

    Rework NIC / Network Reporting
    * Switch to monotonic timer for calculating time deltas between reports. Relying on `DateTime` to calculate time deltas is dangerous as it would be susceptible to phenomena such as a user changing their time or leap seconds. The Stopwatch class allows us to get a monotonic timer if it exists.
    * Interface statistics reset on wake. Previously this would cause nonsensical negative values to be reported. The fix is to reset the internal state when the interface statistics have been reset.
    * Previously, interface hardware already seen when scanned would be re-registered causing zeros to be reported, so the fix is to keep track of what NICs have already been registered
    * Corollary, old interfaces were not closed when removed.
    * Document the code and constants used (eg: "800" is not indicative of converting bytes to bits and a ratio to a percentage). Hopefully it is much clearer.

--- Hardware/Nic/NicGroup.cs
+++ Hardware/Nic/NicGroup.cs
@@ -9,6 +9,8 @@ namespace OpenHardwareMonitor.Hardware.Nic
     {
         private readonly ISettings _settings;
         private List<Nic> _hardware = new List<Nic>();
+        private readonly object _scanLock = new object();
+        private readonly Dictionary<string, Nic> _nics = new Dictionary<string, Nic>();
 
         public NicGroup(ISettings settings)
         {
@@ -20,10 +22,39 @@ namespace OpenHardwareMonitor.Hardware.Nic
 
         private void ScanNics(ISettings settings)
         {
-            NetworkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
-            _hardware = NetworkInterfaces.Where(DesiredNetworkType)
-                .Select((x, i) => new Nic(x, settings, i))
-                .ToList();
+            // If no network is marked up (excluding loopback and tunnel) then don't scan
+            // for interfaces.
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {
+                return;
+            }
+
+            // When multiple events fire concurrently, we don't want threads interferring
+            // with others as they manipulate non-thread safe state.
+            lock (_scanLock)
+            {
+                NetworkInterfaces = NetworkInterface.GetAllNetworkInterfaces()
+                    .Where(DesiredNetworkType)
+                    .OrderBy(x => x.Name)
+                    .ToArray();
+
+                var scanned = NetworkInterfaces.ToDictionary(x => x.Id, x => x);
+                var newNics = scanned.Where(x => !_nics.ContainsKey(x.Key));
+                var removedNics = _nics.Where(x => !  scanned.ContainsKey(x.Key)).ToList();
+
+                foreach (var nic in removedNics)
+                {
+                    nic.Value.Close();
+                    _nics.Remove(nic.Key);
+                }
+
+                foreach (var nic in newNics.Select((x, i) => new { Id = x.Key, Nic = new Nic(x.Value, settings, i) }))
+                {
+                    _nics.Add(nic.Id, nic.Nic);
+                }
+
+                _hardware = _nics.Values.OrderBy(x => x.Name).ToList();
+            }
         }
 
         private void NetworkChange_NetworkAddressChanged(object sender, System.EventArgs e)
