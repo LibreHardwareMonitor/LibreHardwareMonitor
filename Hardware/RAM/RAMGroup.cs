16e1808bbff2de674a2c699bf98b879f6aea40cf
aquacomputer <info@aqua-computer.de>
2019-07-15 14:03:11 +0200

    RAM Code Refactoring Added Virtual Memory

--- Hardware/RAM/RAMGroup.cs
+++ Hardware/RAM/RAMGroup.cs
@@ -1,12 +1,9 @@
-﻿/*
- 
-  This Source Code Form is subject to the terms of the Mozilla Public
-  License, v. 2.0. If a copy of the MPL was not distributed with this
-  file, You can obtain one at http://mozilla.org/MPL/2.0/.
- 
-  Copyright (C) 2012-2013 Michael Möller <mmoeller@openhardwaremonitor.org>
-	
-*/
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Copyright (C) 2011-2012 Michael Möller <mmoeller@openhardwaremonitor.org>
+// Copyright (C) 2016-2019 Sebastian Grams <https://github.com/sebastian-dev>
+// Copyright (C) 2016-2019 Aqua Computer <https://github.com/aquacomputer, info@aqua-computer.de>
 
 using System;
 using System.Collections.Generic;
@@ -19,12 +16,10 @@ namespace OpenHardwareMonitor.Hardware.RAM {
     public RAMGroup(SMBIOS smbios, ISettings settings) {
 
       // No implementation for RAM on Unix systems
-
       if (Software.OperatingSystem.IsLinux) {
         hardware = new Hardware[0];
         return;
       }
-
       hardware = new Hardware[] { new GenericRAM("Generic Memory", settings) };
     }
 
---
+++ Hardware/RAM/RAMInterop.cs
@@ -0,0 +1,34 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Copyright (C) 2011-2012 Michael Möller <mmoeller@openhardwaremonitor.org>
+// Copyright (C) 2016-2019 Sebastian Grams <https://github.com/sebastian-dev>
+// Copyright (C) 2016-2019 Aqua Computer <https://github.com/aquacomputer, info@aqua-computer.de>
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace OpenHardwareMonitor.Hardware.RAM {
+  public class Interop {
+    public const string KERNEL = "kernel32.dll";
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MemoryStatusEx {
+      public uint Length;
+      public uint MemoryLoad;
+      public ulong TotalPhysicalMemory;
+      public ulong AvailablePhysicalMemory;
+      public ulong TotalPageFile;
+      public ulong AvailPageFile;
+      public ulong TotalVirtual;
+      public ulong AvailVirtual;
+      public ulong AvailExtendedVirtual;
+    }
+
+    [DllImport(KERNEL, CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool GlobalMemoryStatusEx(ref MemoryStatusEx buffer);
+  }
+}
