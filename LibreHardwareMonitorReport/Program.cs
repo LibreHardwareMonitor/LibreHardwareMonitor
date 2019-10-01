002104620cf23ddcc67ca27050f076501fd637f0
Rafael Rodrigo Moreira Rosa <rafaelrmrr@hotmail.com>
2019-09-30 22:20:26 -0300

    Including OpenHardwareMonitorReport (now LibreHardwareMonitorReport), from Pull Request #1000. Adjusted to LibreHardwareMonitor.

---
+++ LibreHardwareMonitorReport/Program.cs
@@ -0,0 +1,53 @@
+﻿using LibreHardwareMonitor.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreHardwareMonitorReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer();
+
+            computer.IsCpuEnabled = true;
+            computer.IsGpuEnabled = true;
+            computer.IsStorageEnabled = true;
+            computer.IsMotherboardEnabled = true;
+            computer.IsMemoryEnabled = true;
+            computer.IsControllerEnabled = true;
+            computer.IsNetworkEnabled = true;
+
+            computer.Open();
+
+            computer.Accept(new UpdateVisitor());
+
+            Console.Out.Write(computer.GetReport());
+
+            computer.Close();
+
+        }
+    }
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware)
+                subHardware.Accept(this);
+        }
+
+        public void VisitSensor(ISensor sensor) { }
+
+        public void VisitParameter(IParameter parameter) { }
+    }
+
+}
---
+++ LibreHardwareMonitorReport/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("LibreHardwareMonitorReport")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LibreHardwareMonitorReport")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("5fe2376a-35c0-48b5-9213-766051866248")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
