47782eb00f5ea6aa4785afef5c57cece4c38c1d7
NeoAcheron <master@NeoAcheron.com>
2020-05-24 20:13:09 +0200

    [NZXT Kraken] Fixed the indentation in the group file switch block.

--- LibreHardwareMonitorLib/Hardware/Controller/Nzxt/NzxtGroup.cs
+++ LibreHardwareMonitorLib/Hardware/Controller/Nzxt/NzxtGroup.cs
@@ -24,21 +24,21 @@ namespace LibreHardwareMonitor.Hardware.Controller.Nzxt
                 switch (dev.ProductID)
                 {
                     case 0x2007:
-                        {
-                            var device = new KrakenX3(dev, settings);
-                            _report.AppendLine($"Device name: {productName}");
-                            _report.AppendLine($"Firmware version: {device.FirmwareVersion}");
-                            _report.AppendLine($"{device.Status}");
-                            _report.AppendLine();
-                            _hardware.Add(device);
-                            break;
-                        }
+                    {
+                        var device = new KrakenX3(dev, settings);
+                        _report.AppendLine($"Device name: {productName}");
+                        _report.AppendLine($"Firmware version: {device.FirmwareVersion}");
+                        _report.AppendLine($"{device.Status}");
+                        _report.AppendLine();
+                        _hardware.Add(device);
+                        break;
+                    }
                     default:
-                        {
-                            _report.AppendLine($"Unknown Hardware PID: {dev.ProductID} Name: {productName}");
-                            _report.AppendLine();
-                            break;
-                        }
+                    {
+                        _report.AppendLine($"Unknown Hardware PID: {dev.ProductID} Name: {productName}");
+                        _report.AppendLine();
+                        break;
+                    }
                 }
             }
 
