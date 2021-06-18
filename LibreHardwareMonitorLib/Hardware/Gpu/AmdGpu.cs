6c8b45a0d34b03f2857058818d95fd9f5dd85d25
PhyxionNL <7643972+PhyxionNL@users.noreply.github.com>
2021-06-18 08:54:19 +0200

    Report all ADL status

--- LibreHardwareMonitorLib/Hardware/Gpu/AmdGpu.cs
+++ LibreHardwareMonitorLib/Hardware/Gpu/AmdGpu.cs
@@ -617,14 +617,8 @@ namespace LibreHardwareMonitor.Hardware.Gpu
                         string pt = ((AtiAdlxx.ADLODNCurrentPowerType)i).ToString();
                         AtiAdlxx.ADLStatus status = AtiAdlxx.ADL2_Overdrive6_CurrentPower_Get(_context, _adapterIndex, (AtiAdlxx.ADLODNCurrentPowerType)i, ref power);
 
-                        if (status == AtiAdlxx.ADLStatus.ADL_OK)
-                        {
-                            r.AppendFormat(" Power[{0}].Value: {1}{2}", pt, power * (1.0f / 0xFF), Environment.NewLine);
-                        }
-                        else
-                        {
-                            r.AppendFormat(" Power[{0}].Status: {1}{2}", pt, status.ToString(), Environment.NewLine);
-                        }
+                        r.AppendFormat(" Power[{0}].Value: {1}{2}", pt, power * (1.0f / 0xFF), Environment.NewLine);
+                        r.AppendFormat(" Power[{0}].Status: {1}{2}", pt, status.ToString(), Environment.NewLine);
                     }
                 }
                 catch (EntryPointNotFoundException)
@@ -650,14 +644,8 @@ namespace LibreHardwareMonitor.Hardware.Gpu
                         int temperature = 0;
                         string tt = ((AtiAdlxx.ADLODNTemperatureType)i).ToString();
                         AtiAdlxx.ADLStatus status = AtiAdlxx.ADL2_OverdriveN_Temperature_Get(_context, _adapterIndex, (AtiAdlxx.ADLODNTemperatureType)i, ref temperature);
-                        if (status == AtiAdlxx.ADLStatus.ADL_OK)
-                        {
-                            r.AppendFormat(" Temperature[{0}].Value: {1}{2}", tt, 0.001f * temperature, Environment.NewLine);
-                        }
-                        else
-                        {
-                            r.AppendFormat(" Temperature[{0}].Status: {1}{2}", tt, status.ToString(), Environment.NewLine);
-                        }
+                        r.AppendFormat(" Temperature[{0}].Value: {1}{2}", tt, 0.001f * temperature, Environment.NewLine);
+                        r.AppendFormat(" Temperature[{0}].Status: {1}{2}", tt, status.ToString(), Environment.NewLine);
                     }
                 }
                 catch (EntryPointNotFoundException)
@@ -720,21 +708,14 @@ namespace LibreHardwareMonitor.Hardware.Gpu
                 {
                     var data = new AtiAdlxx.ADLPMLogDataOutput();
                     AtiAdlxx.ADLStatus status = AtiAdlxx.ADL2_New_QueryPMLogData_Get(_context, _adapterIndex, ref data);
-                    if (status == AtiAdlxx.ADLStatus.ADL_OK)
+                    r.Append(" Status: ");
+                    r.AppendLine(status.ToString());
+
+                    for (int i = 0; i < data.sensors.Length; i++)
                     {
-                        for (int i = 0; i < data.sensors.Length; i++)
-                        {
-                            if (data.sensors[i].supported != 0)
-                            {
-                                string st = ((AtiAdlxx.ADLSensorType)i).ToString();
-                                r.AppendFormat(" Sensor[{0}].Value: {1}{2}", st, data.sensors[i].value, Environment.NewLine);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        r.Append(" Status: ");
-                        r.AppendLine(status.ToString());
+                        string st = ((AtiAdlxx.ADLSensorType)i).ToString();
+                        r.AppendFormat(" Sensor[{0}].Value: {1}{2}", st, data.sensors[i].value, Environment.NewLine);
+                        r.AppendFormat(" Sensor[{0}].Supported: {1}{2}", st, data.sensors[i].supported, Environment.NewLine);
                     }
                 }
                 catch (EntryPointNotFoundException)
