719d8adedbb71247a2c62e2b235d08986fb27972
Pomianowski <kontakt@rapiddev.pl>
2021-10-16 21:44:16 +0200

    Update Software namespace docs

--- LibreHardwareMonitorLib/Software/OperatingSystem.cs
+++ LibreHardwareMonitorLib/Software/OperatingSystem.cs
@@ -8,8 +8,14 @@ using System;
 
 namespace LibreHardwareMonitor.Software
 {
+    /// <summary>
+    /// Contains basic information about the operating system.
+    /// </summary>
     public static class OperatingSystem
     {
+        /// <summary>
+        /// Statically checks if the current system <see cref="Is64Bit"/> and <see cref="IsUnix"/>.
+        /// </summary>
         static OperatingSystem()
         {
             // The operating system doesn't change during execution so let's query it just one time.
@@ -20,8 +26,14 @@ namespace LibreHardwareMonitor.Software
                 Is64Bit = true;
         }
 
+        /// <summary>
+        /// Gets information about whether the current system is 64 bit.
+        /// </summary>
         public static bool Is64Bit { get; }
 
+        /// <summary>
+        /// Gets information about whether the current system is Unix based.
+        /// </summary>
         public static bool IsUnix { get; }
     }
 }
