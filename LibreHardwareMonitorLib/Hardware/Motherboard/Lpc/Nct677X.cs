2e3d11637483fbecb7d77b2f63f446f2d74e25fd
DESKTOP-9B96IEA\intos <intosite@gmail.com>
2022-11-10 21:02:48 +0800

    Fixed Voltage reading error.

--- LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/Nct677X.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/Nct677X.cs
@@ -418,8 +418,7 @@ internal class Nct677X : ISuperIO
 
         for (int i = 0; i < Voltages.Length; i++)
         {
-            // if (Chip != Chip.NCT6687D)
-            if (Chip is not Chip.NCT6687D and Chip.NCT6686D)
+            if (Chip is not Chip.NCT6687D and not Chip.NCT6686D)
             {
                 float value = 0.008f * ReadByte(_voltageRegisters[i]);
                 bool valid = value > 0;
