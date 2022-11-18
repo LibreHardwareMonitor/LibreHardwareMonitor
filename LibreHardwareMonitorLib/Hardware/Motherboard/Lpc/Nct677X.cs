557ffedee0c23749152a2b14b6f4e2c84d25423f
PhyxionNL <7643972+PhyxionNL@users.noreply.github.com>
2022-11-18 17:25:13 +0100

    Pattern matching.

--- LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/Nct677X.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/Nct677X.cs
@@ -847,7 +847,7 @@ internal class Nct677X : ISuperIO
 
     private bool IsNuvotonVendor()
     {
-        return Chip == Chip.NCT6687D || Chip == Chip.NCT6683D || Chip == Chip.NCT6686D || ((ReadByte(VENDOR_ID_HIGH_REGISTER) << 8) | ReadByte(VENDOR_ID_LOW_REGISTER)) == NUVOTON_VENDOR_ID;
+        return Chip is Chip.NCT6683D or Chip.NCT6686D or Chip.NCT6687D  || ((ReadByte(VENDOR_ID_HIGH_REGISTER) << 8) | ReadByte(VENDOR_ID_LOW_REGISTER)) == NUVOTON_VENDOR_ID;
     }
 
     private void SaveDefaultFanControl(int index)
