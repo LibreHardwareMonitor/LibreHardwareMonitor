e8b40f5c30ed5af2e1a7de9791e47f681109aef6
Chris Rosenhain <chris@rosenha.in>
2023-06-12 17:09:08 +0930

    Adding support for the ROG CROSSHAIR X670E GENE

--- LibreHardwareMonitorLib/Hardware/Motherboard/Identification.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Identification.cs
@@ -153,6 +153,8 @@ internal class Identification
                 return Model.ROG_CROSSHAIR_VIII_IMPACT;
             case var _ when name.Equals("ROG CROSSHAIR X670E EXTREME", StringComparison.OrdinalIgnoreCase):
                 return Model.ROG_CROSSHAIR_X670E_EXTREME;
+            case var _ when name.Equals("ROG CROSSHAIR X670E GENE", StringComparison.OrdinalIgnoreCase):
+                return Model.ROG_CROSSHAIR_X670E_GENE;
             case var _ when name.Equals("M2N-SLI DELUXE", StringComparison.OrdinalIgnoreCase):
                 return Model.M2N_SLI_Deluxe;
             case var _ when name.Equals("M4A79XTD EVO", StringComparison.OrdinalIgnoreCase):
