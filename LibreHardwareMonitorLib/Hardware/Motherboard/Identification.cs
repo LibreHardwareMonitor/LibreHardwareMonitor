f8afd3de281b988d9631a699b4e7eda78004ac79
DrLippe <echterbeck.philipp@gmail.com>
2023-07-22 10:41:44 +0200

    Add board support for ASUS Z790-I GAMING WIFI

--- LibreHardwareMonitorLib/Hardware/Motherboard/Identification.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Identification.cs
@@ -405,6 +405,8 @@ internal class Identification
                 return Model.Z790_Taichi;
             case var _ when name.Equals("B660M DS3H AX DDR4",StringComparison.OrdinalIgnoreCase):
                 return Model.B660M_DS3H_AX_DDR4;
+            case var _ when name.Equals("ROG STRIX Z790-I GAMING WIFI", StringComparison.OrdinalIgnoreCase):
+                return Model.ROG_STRIX_Z790_I_GAMING_WIFI;
             case var _ when name.Equals("Base Board Product Name", StringComparison.OrdinalIgnoreCase):
             case var _ when name.Equals("To be filled by O.E.M.", StringComparison.OrdinalIgnoreCase):
                 return Model.Unknown;
