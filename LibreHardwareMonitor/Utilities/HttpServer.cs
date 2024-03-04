aab0ca8b1f55ddd6f324a9cbf2769535ef0bf673
wlkmanist <vlad.king5555@mail.ru>
2024-03-04 19:10:29 +0300

    Add Humidity Sensors support
    Signed-off-by: wlkmanist <vlad.king5555@mail.ru>

--- LibreHardwareMonitor/Utilities/HttpServer.cs
+++ LibreHardwareMonitor/Utilities/HttpServer.cs
@@ -679,6 +679,8 @@ public class HttpServer
                 return "loudspeaker.png";
             case SensorType.Throughput:
                 return "throughput.png";
+            case SensorType.Humidity:
+                return "flow.png";
             default:
                 return "power.png";
         }
