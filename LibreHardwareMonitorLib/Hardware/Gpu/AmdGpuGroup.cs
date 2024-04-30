2548671ee492cd7e2fd10f5aec0371d369548fe3
Emerson Pinter <e@pinter.dev>
2024-04-29 22:11:39 -0300

    Avoid multiple initializations of AmdGpu
    ADL finds multiple logical gpus, this commit avoids multiple
    unneeded initializations of AmdGpu class. The main control is moved to
    AmdGpuGroup and the ADL_* calls are updated to ADL2_*, using the
    context. Adapters using PMlog should be initialized only if sensors are
    detected, adapters that pmlog is not enabled or doesn't work, should
    fallback previous logic.

--- LibreHardwareMonitorLib/Hardware/Gpu/AmdGpuGroup.cs
+++ LibreHardwareMonitorLib/Hardware/Gpu/AmdGpuGroup.cs
@@ -15,6 +15,7 @@ namespace LibreHardwareMonitor.Hardware.Gpu;
 
 internal class AmdGpuGroup : IGroup
 {
+    private readonly IntPtr _context = IntPtr.Zero;
     private readonly List<AmdGpu> _hardware = new();
     private readonly StringBuilder _report = new();
     private readonly AtiAdlxx.ADLStatus _status;
@@ -23,7 +24,7 @@ internal class AmdGpuGroup : IGroup
     {
         try
         {
-            _status = AtiAdlxx.ADL_Main_Control_Create(1);
+            _status = AtiAdlxx.ADL2_Main_Control_Create(AtiAdlxx.Main_Memory_Alloc, 1, ref _context);
 
             _report.AppendLine("AMD Display Library");
             _report.AppendLine();
@@ -34,7 +35,7 @@ internal class AmdGpuGroup : IGroup
             if (_status == AtiAdlxx.ADLStatus.ADL_OK)
             {
                 int numberOfAdapters = 0;
-                AtiAdlxx.ADL_Adapter_NumberOfAdapters_Get(ref numberOfAdapters);
+                AtiAdlxx.ADL2_Adapter_NumberOfAdapters_Get(_context, ref numberOfAdapters);
 
                 _report.Append("Number of adapters: ");
                 _report.AppendLine(numberOfAdapters.ToString(CultureInfo.InvariantCulture));
@@ -45,15 +46,23 @@ internal class AmdGpuGroup : IGroup
                     List<AmdGpu> potentialHardware = new();
 
                     AtiAdlxx.ADLAdapterInfo[] adapterInfo = new AtiAdlxx.ADLAdapterInfo[numberOfAdapters];
-                    if (AtiAdlxx.ADL_Adapter_AdapterInfo_Get(adapterInfo) == AtiAdlxx.ADLStatus.ADL_OK)
+                    if (AtiAdlxx.ADL2_Adapter_AdapterInfo_Get(ref _context, adapterInfo) == AtiAdlxx.ADLStatus.ADL_OK)
                     {
                         for (int i = 0; i < numberOfAdapters; i++)
                         {
-                            AtiAdlxx.ADL_Adapter_Active_Get(adapterInfo[i].AdapterIndex, out int isActive);
+                            uint device = 0;
+                            AtiAdlxx.ADLGcnInfo gcnInfo = new();
+                            AtiAdlxx.ADLPMLogSupportInfo pmLogSupportInfo = new();
+                            AtiAdlxx.ADL2_Adapter_Active_Get(_context, adapterInfo[i].AdapterIndex, out int isActive);
+
+                            if (AtiAdlxx.ADL_Method_Exists(nameof(AtiAdlxx.ADL2_GcnAsicInfo_Get)))
+                            {
+                                AtiAdlxx.ADL2_GcnAsicInfo_Get(_context, adapterInfo[i].AdapterIndex, ref gcnInfo);
+                            }
 
                             int adapterId = -1;
-                            if (AtiAdlxx.ADL_Method_Exists(nameof(AtiAdlxx.ADL_Adapter_ID_Get)))
-                                AtiAdlxx.ADL_Adapter_ID_Get(adapterInfo[i].AdapterIndex, out adapterId);
+                            if (AtiAdlxx.ADL_Method_Exists(nameof(AtiAdlxx.ADL2_Adapter_ID_Get)))
+                                AtiAdlxx.ADL2_Adapter_ID_Get(_context, adapterInfo[i].AdapterIndex, out adapterId);
 
                             _report.Append("AdapterIndex: ");
                             _report.AppendLine(i.ToString(CultureInfo.InvariantCulture));
@@ -77,9 +86,43 @@ internal class AmdGpuGroup : IGroup
                             _report.AppendLine(adapterInfo[i].FunctionNumber.ToString(CultureInfo.InvariantCulture));
                             _report.Append("AdapterID: 0x");
                             _report.AppendLine(adapterId.ToString("X", CultureInfo.InvariantCulture));
+                            _report.AppendLine("Family: " + gcnInfo.ASICFamilyId);
 
-                            if (!string.IsNullOrEmpty(adapterInfo[i].UDID) && adapterInfo[i].VendorID == AtiAdlxx.ATI_VENDOR_ID)
-                                potentialHardware.Add(new AmdGpu(adapterInfo[i], settings));
+                            int sensorsSupported = 0;
+                            if (AtiAdlxx.UsePmLogForFamily(gcnInfo.ASICFamilyId) &&
+                                AtiAdlxx.ADL_Method_Exists(nameof(AtiAdlxx.ADL2_Adapter_PMLog_Support_Get)) &&
+                                AtiAdlxx.ADL_Method_Exists(nameof(AtiAdlxx.ADL2_Device_PMLog_Device_Create)))
+                            {
+                                if (AtiAdlxx.ADLStatus.ADL_OK == AtiAdlxx.ADL2_Device_PMLog_Device_Create(_context, adapterInfo[i].AdapterIndex, ref device) &&
+                                    AtiAdlxx.ADLStatus.ADL_OK == AtiAdlxx.ADL2_Adapter_PMLog_Support_Get(_context, adapterInfo[i].AdapterIndex, ref pmLogSupportInfo))
+                                {
+                                    int k = 0;
+                                    while (pmLogSupportInfo.usSensors[k] != (ushort)AtiAdlxx.ADLPMLogSensors.ADL_SENSOR_MAXTYPES)
+                                    {
+                                        k++;
+                                    }
+                                    sensorsSupported = k;
+                                }
+                                _report.AppendLine("Sensors Supported: " + sensorsSupported);
+
+                                if (device != 0)
+                                {
+                                    AtiAdlxx.ADL2_Device_PMLog_Device_Destroy(_context, device);
+                                }
+                            }
+
+                            if (!string.IsNullOrEmpty(adapterInfo[i].UDID) && adapterInfo[i].VendorID == AtiAdlxx.ATI_VENDOR_ID &&
+                                !IsAlreadyAdded(adapterInfo[i].BusNumber, adapterInfo[i].DeviceNumber))
+                            {
+                                if (sensorsSupported > 0)
+                                {
+                                    _hardware.Add(new AmdGpu(_context, adapterInfo[i], gcnInfo, settings));
+                                }
+                                else
+                                {
+                                    potentialHardware.Add(new AmdGpu(_context, adapterInfo[i], gcnInfo, settings));
+                                }
+                            }
 
                             _report.AppendLine();
                         }
@@ -88,7 +131,7 @@ internal class AmdGpuGroup : IGroup
                     foreach (IGrouping<string, AmdGpu> amdGpus in potentialHardware.GroupBy(x => $"{x.BusNumber}-{x.DeviceNumber}"))
                     {
                         AmdGpu amdGpu = amdGpus.OrderByDescending(x => x.Sensors.Length).FirstOrDefault();
-                        if (amdGpu != null)
+                        if (amdGpu != null && !IsAlreadyAdded(amdGpu.BusNumber, amdGpu.DeviceNumber))
                             _hardware.Add(amdGpu);
                     }
                 }
@@ -102,6 +145,18 @@ internal class AmdGpuGroup : IGroup
             _report.AppendLine(e.ToString());
             _report.AppendLine();
         }
+    }
+
+    private bool IsAlreadyAdded(int busNumber, int deviceNumber)
+    {
+        foreach (AmdGpu g in _hardware)
+        {
+            if (g.BusNumber == busNumber && g.DeviceNumber == deviceNumber)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     public IReadOnlyList<IHardware> Hardware => _hardware;
@@ -118,8 +173,8 @@ internal class AmdGpuGroup : IGroup
             foreach (AmdGpu gpu in _hardware)
                 gpu.Close();
 
-            if (_status == AtiAdlxx.ADLStatus.ADL_OK)
-                AtiAdlxx.ADL_Main_Control_Destroy();
+            if (_status == AtiAdlxx.ADLStatus.ADL_OK && _context != IntPtr.Zero)
+                AtiAdlxx.ADL2_Main_Control_Destroy(_context);
         }
         catch (Exception)
         { }
