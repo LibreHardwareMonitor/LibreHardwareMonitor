af7fca13978ca9f69d12abf123ca036bc8fbb50b
Ezra Indrastata <pwnedgod@users.noreply.github.com>
2024-06-12 00:28:37 +0900

    Fix missing view for IT8792E and allow fan control for IT8792E on some Gigabyte AMD boards (#1313)
    * Check before entering bugged 0x4E IT87
    
    * Specify fan control registers for IT8792E
    
    * Refactor chip ID reading using TryReadWord
    
    * Introduce ECIO gigabyte EC control
    
    * Add ECIO compatibility check
    
    * Refactor to consider result of read/write
    
    * Refactor styles and syntaxes
    
    * Rename SMFI to Smfi
    
    * Remove public modifier for consistency
    
    * Update code style
    
    * Increase timeout and remove sleep for ecio waits
    
    * Remove extra WaitIBE

---
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/IGigabyteController.cs
@@ -0,0 +1,14 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Copyright (C) LibreHardwareMonitor and Contributors.
+// Partial Copyright (C) Michael Möller <mmoeller@openhardwaremonitor.org> and Contributors.
+// All Rights Reserved.
+
+namespace LibreHardwareMonitor.Hardware.Motherboard.Lpc;
+
+internal interface IGigabyteController
+{
+    bool Enable(bool enabled);
+
+    void Restore();
+}
