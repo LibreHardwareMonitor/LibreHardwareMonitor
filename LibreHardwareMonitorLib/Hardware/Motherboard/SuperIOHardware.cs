b47afdf571a24e5cc46ef71acbcb2cc1e5fa6258
Tellinq <59412384+Tellinq@users.noreply.github.com>
2024-07-20 10:47:00 -0400

    Add proper definition support for all Gigabyte B550 models (#1360)
    * Add Gigabyte B550 AORUS Elite (v1/v2) models
    
    * Add B550 Gaming X (v1 and v2) models as their layout is identical too
    
    * Add B550 AORUS Pro v2
    
    * Add B550M AORUS Elite
    
    * Add the rest of Gigabyte B550 and B550M (Except the Vision D, Aorus Master, B550M Aorus Pro models)
    
    * Add B550 UD AC Y1
    
    System integrators such as iBuyPower and CyberPowerPC have sold systems with this OEM branded model, and thus would not be detected within this PR otherwise.
    
    * Add B550M Pro, B550 Aorus Master, B550 Vision D models
    
    * Add B550I AORUS PRO AX
    
    * Rename EC_TEMP# to a more display friendly name

--- LibreHardwareMonitorLib/Hardware/Motherboard/SuperIOHardware.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/SuperIOHardware.cs
@@ -1818,7 +1818,11 @@ internal sealed class SuperIOHardware : Hardware
 
                         break;
 
+                    case Model.B550_AORUS_MASTER:
                     case Model.B550_AORUS_PRO:
+                    case Model.B550_AORUS_PRO_AC:
+                    case Model.B550_AORUS_PRO_AX:
+                    case Model.B550_VISION_D:
                         v.Add(new Voltage("Vcore", 0, 0, 1));
                         v.Add(new Voltage("+3.3V", 1, 6.5F, 10));
                         v.Add(new Voltage("+12V", 2, 5, 1));
@@ -1829,11 +1833,11 @@ internal sealed class SuperIOHardware : Hardware
                         v.Add(new Voltage("+3V Standby", 7, 10, 10));
                         v.Add(new Voltage("CMOS Battery", 8, 10, 10));
                         t.Add(new Temperature("System #1", 0));
-                        t.Add(new Temperature("Chipset", 1));
+                        t.Add(new Temperature("External #1", 1));
                         t.Add(new Temperature("CPU", 2));
                         t.Add(new Temperature("PCIe x16", 3));
-                        t.Add(new Temperature("VRM", 4));
-                        t.Add(new Temperature("PCH", 5));
+                        t.Add(new Temperature("VRM MOS", 4));
+                        t.Add(new Temperature("Chipset", 5));
                         f.Add(new Fan("CPU Fan", 0));
                         f.Add(new Fan("System Fan #1", 1));
                         f.Add(new Fan("System Fan #2", 2));
@@ -1844,6 +1848,70 @@ internal sealed class SuperIOHardware : Hardware
                         c.Add(new Control("System Fan #2", 2));
                         c.Add(new Control("System Fan #3", 3));
                         c.Add(new Control("CPU Optional Fan", 4));
+
+                        break;
+
+                    case Model.B550_AORUS_ELITE:
+                    case Model.B550_AORUS_ELITE_AX:
+                    case Model.B550_GAMING_X:
+                    case Model.B550_UD_AC:
+                    case Model.B550M_AORUS_PRO:
+                    case Model.B550M_AORUS_PRO_AX:
+                        v.Add(new Voltage("Vcore", 0, 0, 1));
+                        v.Add(new Voltage("+3.3V", 1, 6.5F, 10));
+                        v.Add(new Voltage("+12V", 2, 5, 1));
+                        v.Add(new Voltage("+5V", 3, 1.5F, 1));
+                        v.Add(new Voltage("Vcore SoC", 4, 0, 1));
+                        v.Add(new Voltage("VDDP", 5, 0, 1));
+                        v.Add(new Voltage("DRAM", 6, 0, 1));
+                        v.Add(new Voltage("+3V Standby", 7, 10, 10));
+                        v.Add(new Voltage("CMOS Battery", 8, 10, 10));
+                        t.Add(new Temperature("System #1", 0));
+                        t.Add(new Temperature("System #2", 1));
+                        t.Add(new Temperature("CPU", 2));
+                        t.Add(new Temperature("PCIe x16", 3));
+                        t.Add(new Temperature("VRM MOS", 4));
+                        t.Add(new Temperature("Chipset", 5));
+                        f.Add(new Fan("CPU Fan", 0));
+                        f.Add(new Fan("System Fan #1", 1));
+                        f.Add(new Fan("System Fan #2", 2));
+                        f.Add(new Fan("System Fan #3", 3));
+                        f.Add(new Fan("CPU Optional Fan", 4));
+                        c.Add(new Control("CPU Fan", 0));
+                        c.Add(new Control("System Fan #1", 1));
+                        c.Add(new Control("System Fan #2", 2));
+                        c.Add(new Control("System Fan #3", 3));
+                        c.Add(new Control("CPU Optional Fan", 4));
+
+                        break;
+
+                    case Model.B550I_AORUS_PRO_AX:
+                    case Model.B550M_AORUS_ELITE:
+                    case Model.B550M_GAMING:
+                    case Model.B550M_DS3H:
+                    case Model.B550M_DS3H_AC:
+                    case Model.B550M_S2H:
+                    case Model.B550M_H:
+                        v.Add(new Voltage("Vcore", 0, 0, 1));
+                        v.Add(new Voltage("+3.3V", 1, 6.5F, 10));
+                        v.Add(new Voltage("+12V", 2, 5, 1));
+                        v.Add(new Voltage("+5V", 3, 1.5F, 1));
+                        v.Add(new Voltage("Vcore SoC", 4, 0, 1));
+                        v.Add(new Voltage("VDDP", 5, 0, 1));
+                        v.Add(new Voltage("DRAM", 6, 0, 1));
+                        v.Add(new Voltage("+3V Standby", 7, 10, 10));
+                        v.Add(new Voltage("CMOS Battery", 8, 10, 10));
+                        t.Add(new Temperature("System", 0));
+                        t.Add(new Temperature("VSoC MOS", 1));
+                        t.Add(new Temperature("CPU", 2));
+                        t.Add(new Temperature("VRM MOS", 4));
+                        t.Add(new Temperature("Chipset", 5));
+                        f.Add(new Fan("CPU Fan", 0));
+                        f.Add(new Fan("System Fan #1", 1));
+                        f.Add(new Fan("System Fan #2", 2));
+                        c.Add(new Control("CPU Fan", 0));
+                        c.Add(new Control("System Fan #1", 1));
+                        c.Add(new Control("System Fan #2", 2));
 
                         break;
 
@@ -2053,7 +2121,11 @@ internal sealed class SuperIOHardware : Hardware
                 {
                     case Model.X570_AORUS_MASTER: // IT879XE
                     case Model.X570_AORUS_ULTRA:
+                    case Model.B550_AORUS_MASTER:
                     case Model.B550_AORUS_PRO:
+                    case Model.B550_AORUS_PRO_AC:
+                    case Model.B550_AORUS_PRO_AX:
+                    case Model.B550_VISION_D:
                         v.Add(new Voltage("VIN0", 0));
                         v.Add(new Voltage("DDRVTT AB", 1));
                         v.Add(new Voltage("Chipset Core", 2));
@@ -2064,7 +2136,7 @@ internal sealed class SuperIOHardware : Hardware
                         v.Add(new Voltage("+3V Standby", 7, 1f, 1f));
                         v.Add(new Voltage("CMOS Battery", 8, 1f, 1f));
                         t.Add(new Temperature("PCIe x8", 0));
-                        t.Add(new Temperature("EC_TEMP2", 1));
+                        t.Add(new Temperature("External #2", 1));
                         t.Add(new Temperature("System #2", 2));
                         f.Add(new Fan("System Fan #5 Pump", 0));
                         f.Add(new Fan("System Fan #6 Pump", 1));
