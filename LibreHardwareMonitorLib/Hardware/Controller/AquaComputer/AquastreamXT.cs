9f7200dc78d821663abb2d2ba04695bce1da48f7
Daniel Varga <varga.daniel@gmx.de>
2024-09-26 21:01:12 +0200

    Improve HidDevice Identifier

--- LibreHardwareMonitorLib/Hardware/Controller/AquaComputer/AquastreamXT.cs
+++ LibreHardwareMonitorLib/Hardware/Controller/AquaComputer/AquastreamXT.cs
@@ -26,7 +26,7 @@ internal sealed class AquastreamXT : Hardware
     private readonly Sensor[] _temperatures = new Sensor[3];
     private readonly Sensor[] _voltages = new Sensor[2];
 
-    public AquastreamXT(HidDevice dev, ISettings settings) : base("Aquastream XT", new Identifier(dev.DevicePath), settings)
+    public AquastreamXT(HidDevice dev, ISettings settings) : base("Aquastream XT", new Identifier(dev), settings)
     {
         if (dev.TryOpen(out _stream))
         {
@@ -129,7 +129,7 @@ internal sealed class AquastreamXT : Hardware
         {
             return;
         }
-       
+
 
         if (_rawData[0] != 0x4)
             return;
