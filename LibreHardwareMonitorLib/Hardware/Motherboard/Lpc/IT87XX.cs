facae24df0408e8ec0e42ee4cedc553cc9d2dbe5
thegame4craft <48205247+thegame4craft@users.noreply.github.com>
2024-10-06 13:48:50 +0200

    fix missing sensors on it8665e

--- LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/IT87XX.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/IT87XX.cs
@@ -132,8 +132,8 @@ internal class IT87XX : ISuperIO
             case Chip.IT8665E:
                 Voltages = new float?[9];
                 Temperatures = new float?[6];
-                Fans = new float?[4];
-                Controls = new float?[4];
+                Fans = new float?[6];
+                Controls = new float?[6];
                 _requiresBankSelect = true;
                 break;
 
