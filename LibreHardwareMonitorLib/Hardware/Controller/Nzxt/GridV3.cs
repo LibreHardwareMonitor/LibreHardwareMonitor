1ca4ebca9589fe6f23bc6dfefced51005d29e9d3
Daniel Varga <DaVarga@users.noreply.github.com>
2024-11-24 10:49:34 +0100

    Improve HidDevice Identifier (#1476)

--- LibreHardwareMonitorLib/Hardware/Controller/Nzxt/GridV3.cs
+++ LibreHardwareMonitorLib/Hardware/Controller/Nzxt/GridV3.cs
@@ -33,7 +33,7 @@ internal sealed class GridV3 : Hardware
     private readonly HidStream _stream;
     private readonly Sensor[] _voltages = new Sensor[FANS_COUNT];
 
-    public GridV3(HidDevice dev, ISettings settings) : base("NZXT GRID+ V3", new Identifier(dev.DevicePath), settings)
+    public GridV3(HidDevice dev, ISettings settings) : base("NZXT GRID+ V3", new Identifier(dev), settings)
     {
         if (dev.TryOpen(out _stream))
         {
