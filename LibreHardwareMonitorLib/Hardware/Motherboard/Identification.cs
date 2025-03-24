a252aaefc0e8e7cde6cbbd1abc8137f77240617d
Anthony Nguyen <ahnguyen@gmail.com>
2025-03-24 15:35:58 -0700

    Add ASRock B850M Steel Legend Wifi support
    Adds missing temperature sensors like T_Sensor1 and renamed several items

--- LibreHardwareMonitorLib/Hardware/Motherboard/Identification.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Identification.cs
@@ -621,6 +621,8 @@ internal class Identification
                 return Model.ROG_CROSSHAIR_X870E_HERO;
             case var _ when name.Equals("X11SWN-E", StringComparison.OrdinalIgnoreCase):
                 return Model.X11SWN_E;
+            case var _ when name.Equals("B850M Steel Legend WiFi", StringComparison.OrdinalIgnoreCase):
+                return Model.B850M_STEEL_LEGEND_WIFI;
             case var _ when name.Equals("Base Board Product Name", StringComparison.OrdinalIgnoreCase):
             case var _ when name.Equals("To be filled by O.E.M.", StringComparison.OrdinalIgnoreCase):
                 return Model.Unknown;
