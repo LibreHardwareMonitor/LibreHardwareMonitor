f81e64f53699766fc8478fd023812caa6616bb3b
Rem0o <remi.mercier.software@gmail.com>
2025-08-05 16:25:21 -0400

    Update IT87XX.cs

--- LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/IT87XX.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/IT87XX.cs
@@ -162,8 +162,8 @@ internal class IT87XX : ISuperIO
             case Chip.IT8696E:
                 Voltages = new float?[10];
                 Temperatures = new float?[6];
-                Fans = new float?[5];
-                Controls = new float?[5];
+                Fans = new float?[6];
+                Controls = new float?[6];
                 break;
 
             case Chip.IT87952E:
