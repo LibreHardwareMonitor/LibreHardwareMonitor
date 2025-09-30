bcc197f265f81ea1a2a9f74bb2d355f38975f157
demorfi <demorfi@gmail.com>
2025-10-01 00:03:13 +0700

    Added base support Asus ROG Crosshair X670E Hero and PSU MSI MEG Ai1300P (#1575)
    * Added base support matherboard Asus ROG Crosshair X670E Hero
    
    * Added base support PSU MSI MEG Ai1300P
    
    * Refactoring MSI PSU lib

---
+++ LibreHardwareMonitorLib/Hardware/Psu/Msi/MsiPsu.cs
@@ -0,0 +1,75 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// Copyright (C) 2024 demorfi<demorfi@gmail.com>
+// Copyright (C) LibreHardwareMonitor and Contributors.
+// All Rights Reserved.
+
+using System.Collections.Generic;
+using System.Globalization;
+using HidSharp;
+
+namespace LibreHardwareMonitor.Hardware.Psu.Msi;
+
+internal sealed class MsiPsu : Hardware
+{
+    private readonly HidDevice _device;
+    private readonly List<PsuSensor> _sensors = [];
+
+    public MsiPsu(HidDevice device, ISettings settings, int index)
+        : base("MSI PSU", new Identifier("psu", "msi", index.ToString()), settings)
+    {
+        _device = device;
+        using HidStream stream = device.Open();
+        UsbApi.FirmwareInfo fwInfo = UsbApi.FwInfo(stream);
+        Name = $"{fwInfo.Vendor} {fwInfo.Product}";
+
+        AddSensors(settings);
+    }
+
+    public override HardwareType HardwareType => HardwareType.Psu;
+
+    public override void Update()
+    {
+        using HidStream stream = _device.Open();
+        float[] info = UsbApi.InfoList(stream);
+        _sensors.ForEach(s => s.Update(info));
+    }
+
+    private void AddSensors(ISettings settings)
+    {
+        int sensorIndex = 0;
+        
+        _sensors.Add(new PsuSensor("Case", sensorIndex++, SensorType.Fan, this, settings, UsbApi.IndexInfo.FAN_RPM));
+        _sensors.Add(new PsuSensor("Case", sensorIndex++, SensorType.Temperature, this, settings, UsbApi.IndexInfo.TEMP));
+
+        _sensors.Add(new PsuSensor("+12V", sensorIndex++, SensorType.Voltage, this, settings, UsbApi.IndexInfo.VOLTS_12));
+        _sensors.Add(new PsuSensor("+12V", sensorIndex++, SensorType.Current, this, settings, UsbApi.IndexInfo.AMPS_12));
+
+        _sensors.Add(new PsuSensor("+5V", sensorIndex++, SensorType.Voltage, this, settings, UsbApi.IndexInfo.VOLTS_5));
+        _sensors.Add(new PsuSensor("+5V", sensorIndex++, SensorType.Current, this, settings, UsbApi.IndexInfo.AMPS_5));
+
+        _sensors.Add(new PsuSensor("+3.3V", sensorIndex++, SensorType.Voltage, this, settings, UsbApi.IndexInfo.VOLTS_3V3));
+        _sensors.Add(new PsuSensor("+3.3V", sensorIndex++, SensorType.Current, this, settings, UsbApi.IndexInfo.AMPS_3V3));
+
+        _sensors.Add(new PsuSensor("PSU Efficiency", sensorIndex++, SensorType.Level, this, settings, UsbApi.IndexInfo.EFFICIENCY));
+        _sensors.Add(new PsuSensor("PSU Out", sensorIndex++, SensorType.Power, this, settings, UsbApi.IndexInfo.PSU_OUT));
+        _sensors.Add(new PsuSensor("Total Runtime", sensorIndex++, SensorType.TimeSpan, this, settings, UsbApi.IndexInfo.RUNTIME, true));
+    }
+
+    private class PsuSensor : Sensor
+    {
+        private readonly UsbApi.IndexInfo _indexInfo;
+
+        public PsuSensor(string name, int index, SensorType type, MsiPsu hardware, ISettings settings, UsbApi.IndexInfo indexInfo, bool noHistory = false)
+            : base(name, index, false, type, hardware, null, settings, noHistory)
+        {
+            _indexInfo = indexInfo;
+            hardware.ActivateSensor(this);
+        }
+
+        public void Update(float[] info)
+        {
+            Value = info[(int)_indexInfo];
+        }
+    }
+}
---
+++ LibreHardwareMonitorLib/Hardware/Psu/Msi/MsiPsuGroup.cs
@@ -0,0 +1,64 @@
+﻿// This Source Code Form is subject to the terms of the GNU Public License, v. 2.0.
+// Copyright (C) 2024 demorfi<demorfi@gmail.com>
+// Copyright (C) LibreHardwareMonitor and Contributors.
+// All Rights Reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HidSharp;
+
+namespace LibreHardwareMonitor.Hardware.Psu.Msi;
+
+public class MsiPsuGroup : IGroup
+{
+    private static readonly int[] _productIds =
+    {
+        0x56d4, // MEG Ai1300P
+    };
+
+    private static readonly ushort _vendorId = 0x0db0;
+    private readonly List<IHardware> _hardware;
+    private readonly StringBuilder _report;
+
+    public MsiPsuGroup(ISettings settings)
+    {
+        _report = new StringBuilder();
+        _report.AppendLine("MSI Ai series PSU Hardware");
+        _report.AppendLine();
+
+        _hardware = new List<IHardware>();
+        foreach (HidDevice dev in DeviceList.Local.GetHidDevices(_vendorId))
+        {
+            if (_productIds.Contains(dev.ProductID))
+            {
+                var device = new MsiPsu(dev, settings, _hardware.Count);
+                _hardware.Add(device);
+                _report.AppendLine($"Device name: {device.Name}");
+                _report.AppendLine();
+            }
+        }
+
+        if (_hardware.Count == 0)
+        {
+            _report.AppendLine("No MSI PSU Hardware found.");
+            _report.AppendLine();
+        }
+    }
+
+    public IReadOnlyList<IHardware> Hardware => _hardware;
+
+    public void Close()
+    {
+        foreach (IHardware iHardware in _hardware)
+        {
+            if (iHardware is Hardware hardware)
+                hardware.Close();
+        }
+    }
+
+    public string GetReport()
+    {
+        return _report.ToString();
+    }
+}
