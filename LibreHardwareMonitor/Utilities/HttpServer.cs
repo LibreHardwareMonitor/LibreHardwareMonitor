85877ec50c4c396ba83ecc820c352e9c377a972f
Cristobal E. <criesca@gmail.com>
2025-10-22 01:02:40 -0600

    Skip null values as per #1979

--- LibreHardwareMonitor/Utilities/HttpServer.cs
+++ LibreHardwareMonitor/Utilities/HttpServer.cs
@@ -617,6 +617,13 @@ public class HttpServer
                     string valueSensor = sensor.Text.Replace("#", String.Empty);
                     string tagHardware = (((HardwareNode)node).Hardware.Parent != null ? ((HardwareNode)node).Hardware.Parent.HardwareType.ToString() : ((HardwareNode)node).Hardware.HardwareType.ToString());
 
+                    if (sensor.Sensor.Value == null)
+                    {
+                        // We do not need to do anything if the value is null
+                        responseStr += $"# HELP {tagHardware}_{tagSensorType}:{valueSensor} had a null value and was skipped.\n";
+                        continue;
+                    }
+
                     double _factor = 1;
                     string tagSensorUnits = String.Empty;
                     // Get factor and unit suffix from dictionary ...
