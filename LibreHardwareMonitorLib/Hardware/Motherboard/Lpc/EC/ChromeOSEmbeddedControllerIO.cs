7a0a43c5d813159802d98e17245962fe49241bb5
Stephen Horvath <s.horvath@outlook.com.au>
2025-11-18 22:59:04 +1000

    Migrate CrOS ECs to PawnIO

--- LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/EC/ChromeOSEmbeddedControllerIO.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Lpc/EC/ChromeOSEmbeddedControllerIO.cs
@@ -4,6 +4,8 @@
 // All Rights Reserved.
 
 using System;
+using System.Diagnostics;
+using LibreHardwareMonitor.PawnIo;
 
 namespace LibreHardwareMonitor.Hardware.Motherboard.Lpc.EC;
 
@@ -11,49 +13,32 @@ public class ChromeOSEmbeddedControllerIO : IEmbeddedControllerIO
 {
     private bool _disposed;
 
-    public ushort BasePort { get; }
-
-    private readonly ushort[] EC_LPC_ADDR_MEMMAP = { 0x900, 0xE00 };
-    private const byte EC_MEMMAP_SIZE = 255;
-    private const byte EC_MEMMAP_ID = 0x20;
+    private readonly LpcCrOSEc _pawnModule;
 
     public ChromeOSEmbeddedControllerIO()
     {
+        _pawnModule = new LpcCrOSEc();
+
         if (!Mutexes.WaitEc(10))
         {
             throw new BusMutexLockingFailedException();
         }
-        
-        // Find the EC base port
-        foreach (var port in EC_LPC_ADDR_MEMMAP)
-        {
-            if (Ring0.ReadIoPort((uint)(port + EC_MEMMAP_ID)) == 'E' &&
-                Ring0.ReadIoPort((uint)(port + EC_MEMMAP_ID + 1)) == 'C')
-            {
-                BasePort = port;
-                return;
-            }
-        }
-
-        throw new NotImplementedException("Embedded controller not found");
     }
 
     public void Read(ushort[] registers, byte[] data)
     {
-        if (registers.Length != data.Length)
-        {
-            throw new ArgumentException("Registers and data length mismatch");
-        }
-
-        if (registers.Length > EC_MEMMAP_SIZE)
-        {
-            throw new ArgumentException("Registers length exceeds 255");
-        }
+        Trace.Assert(registers.Length <= data.Length,
+                     "data buffer length has to be greater or equal to the registers array length");
 
         for (int i = 0; i < registers.Length; i++)
         {
-            data[i] = Ring0.ReadIoPort((uint)(BasePort + registers[i]));
+            data[i] = this.ReadMemmap((byte)registers[i], 1)[0];
         }
+    }
+
+    public byte[] ReadMemmap(byte offset, byte bytes)
+    {
+        return _pawnModule.ReadMemmap(offset, bytes);
     }
 
     public void Dispose()
---
+++ LibreHardwareMonitorLib/PawnIo/LpcCrOS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LibreHardwareMonitor.PawnIo;
+
+public class LpcCrOSEc
+{
+    private readonly PawnIo _pawnIO = PawnIo. LoadModuleFromResource(typeof(AmdFamily0F).Assembly, $"{nameof(LibreHardwareMonitor)}.Resources.PawnIo.LpcCrOSEC.bin");
+
+    public byte[] EcCommand(int version, int command, int outsize, int insize, byte[] data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public byte[] ReadMemmap(byte offset, byte bytes)
+    {
+        long[] inArray = new long[2];
+        inArray[0] = offset;
+        inArray[1] = bytes;
+        long[] outArray = _pawnIO.Execute("ioctl_ec_readmem", inArray, (int)Math.Ceiling((double)bytes / 8.0));
+        byte[] retArray = new byte[bytes];
+        Buffer.BlockCopy(outArray, 0, retArray, 0, bytes);
+        return retArray;
+    }
+
+    public void Close() => _pawnIO.Close();
+}
