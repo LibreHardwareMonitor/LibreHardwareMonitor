32d8c5210ff2db3f39a2f1d12a24b50914d8756d
Timotej <20747466+Timic3@users.noreply.github.com>
2026-01-18 17:48:08 +0100

    Clean up imports

--- LibreHardwareMonitorLib/Hardware/Gpu/NvidiaGpu.cs
+++ LibreHardwareMonitorLib/Hardware/Gpu/NvidiaGpu.cs
@@ -8,8 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Runtime;
-using System.Runtime.InteropServices;
 using System.Text;
 using LibreHardwareMonitor.Interop;
 using Microsoft.Win32;
