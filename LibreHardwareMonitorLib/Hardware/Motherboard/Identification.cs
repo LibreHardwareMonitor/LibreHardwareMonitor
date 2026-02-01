f05bdf44499379b2449fafd30e1a58ca20aa140d
Alcolawl <5004749+Alcolawl@users.noreply.github.com>
2026-02-01 14:23:08 -0500

    Add support for MSI PRO B850M-A Project Zero (MS-7E78)

--- LibreHardwareMonitorLib/Hardware/Motherboard/Identification.cs
+++ LibreHardwareMonitorLib/Hardware/Motherboard/Identification.cs
@@ -693,6 +693,8 @@ internal class Identification
                 return Model.B850S_PRO_WIFI6E;
             case var _ when name.Equals("PRO B850M-A WIFI (MS-7E66)", StringComparison.OrdinalIgnoreCase):
                 return Model.B850MA_PRO_WIFI;
+            case var _ when name.Equals("PRO B850M-A WIFI PZ (MS-7E78)", StringComparison.OrdinalIgnoreCase):
+                return Model.B850MA_PRO_WIFI_PZ;
             case var _ when name.Equals("PRO B850M-P WIFI (MS-7E71)", StringComparison.OrdinalIgnoreCase):
                 return Model.B850MP_PRO_WIFI;
             case var _ when name.Equals("B850 GAMING PLUS WIFI (MS-7E56)", StringComparison.OrdinalIgnoreCase):
